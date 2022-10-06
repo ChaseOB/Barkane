5fece7787781e8e0450550038a083dea7e1c7bfc
rmoon45 <113371188+rmoon45@users.noreply.github.com>
2022-10-05 22:19:48 -0400

    asdf

--- Barkane/Assets/Scripts/UI/MainMenuManager.cs
+++ Barkane/Assets/Scripts/UI/MainMenuManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour
+public class MainMenuManager : MonoBehaviour
 {
     public int gameStartScene;
 
