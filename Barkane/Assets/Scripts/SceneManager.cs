ee4f0f7520336d3cfdaa41fa32a21d0cf5bf31e2
Sophia <sophialin00000@gmail.com>
2022-10-08 15:11:54 -0400

    Add skybox swapping and remove marmalade

--- Barkane/Assets/Scripts/SceneManager.cs
+++ Barkane/Assets/Scripts/SceneManager.cs
@@ -4,49 +4,37 @@ using UnityEngine;
 
 public class SceneManager : MonoBehaviour
 {
-    public GameObject firstLevel;
-    private GameObject marmalade;
-    private GameObject instantiatedMarmalade;
-    private GameObject prefab;
-    private GameObject instantiatedPrefab;
+    private GameObject level;
+    private GameObject instantiatedLevel;
 
-    void Awake() {
-        instantiatedPrefab = Instantiate(firstLevel, transform.position, Quaternion.identity);
-        marmalade = (GameObject) Resources.Load("Prefabs/Player");
-        instantiatedMarmalade = Instantiate(marmalade, transform.position, Quaternion.identity);
+    public void SwitchLevel(GameObject level) {
+        this.level = level;
+        if (instantiatedLevel != null) {
+            Destroy(instantiatedLevel);
+        }
+        instantiatedLevel = Instantiate(level, transform.position, Quaternion.identity);
     }
 
-    IEnumerator Start()
-    {
-        Debug.Log("Start1");
-        yield return new WaitForSeconds(5);
-        Debug.Log("Start2");
-        Destroy(instantiatedMarmalade);
-        yield return new WaitForSeconds(10);
-        Debug.Log("Start3");
-        instantiatedMarmalade = Instantiate(marmalade, transform.position, Quaternion.identity);
-    }
-
-    public void SwitchLevel(GameObject prefab) {
-        this.prefab = prefab;
-        Destroy(instantiatedPrefab);
-        instantiatedPrefab = Instantiate(prefab, transform.position, Quaternion.identity);
-        Destroy(instantiatedMarmalade);
-        instantiatedMarmalade = Instantiate(marmalade, transform.position, Quaternion.identity);
-    }
-
-    public void SwitchLevel(string prefabString) {
-        prefab = (GameObject) Resources.Load("Prefabs/" + prefabString);
-        Destroy(instantiatedPrefab);
-        instantiatedPrefab = Instantiate(prefab, transform.position, Quaternion.identity);
-        Destroy(instantiatedMarmalade);
-        instantiatedMarmalade = Instantiate(marmalade, transform.position, Quaternion.identity);
+    public void SwitchLevel(string level) {
+        this.level = (GameObject) Resources.Load("Prefabs/" + level);
+        if (instantiatedLevel != null) {
+            Destroy(instantiatedLevel);
+        }
+        instantiatedLevel = Instantiate(this.level, transform.position, Quaternion.identity);
     }
 
     public void ResetLevel() {
-        Destroy(instantiatedPrefab);
-        Destroy(instantiatedMarmalade);
-        instantiatedPrefab = Instantiate(prefab, transform.position, Quaternion.identity);
-        instantiatedMarmalade = Instantiate(marmalade, transform.position, Quaternion.identity);
+        if (instantiatedLevel != null) {
+            Destroy(instantiatedLevel);
+            instantiatedLevel = Instantiate(level, transform.position, Quaternion.identity);
+        }
+    }
+
+    public void ChangeSkybox(Material skybox) {
+        RenderSettings.skybox = skybox;
+    }
+
+    public void ChangeSkybox(string skybox) {
+        RenderSettings.skybox = (Material) Resources.Load(skybox);
     }
 }
