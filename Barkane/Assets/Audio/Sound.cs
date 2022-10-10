8995c81163f33a2e83e2e8adf7626fab1e968594
mi-chel <michelle.mei.zhang@gmail.com>
2022-10-10 18:03:55 -0400

    Added audio manager

---
+++ Barkane/Assets/Audio/Sound.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+    public AudioClip clip;
+    [Range(0f,1f)]
+    public float volume;
+    [Range(.1f,.3f)]
+    public float pitch;
+    public bool loop;
+    public bool doRandomPitch;
+
+    [HideInInspector]
+    public AudioSource source;
+}
