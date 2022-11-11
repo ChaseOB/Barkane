0e91096847554cc40b3da3dc792c89ea48f1729e
alanrtao <alanrtao@outlook.com>
2022-11-10 22:05:22 -0500

    actual bending

--- Barkane/Assets/Scripts/Mechanics/GlowStick.cs
+++ Barkane/Assets/Scripts/Mechanics/GlowStick.cs
@@ -19,26 +19,18 @@ public class GlowStick : MonoBehaviour
 
     [SerializeField] private SquareRenderSettings squareRenderSettings;
 
-    public GameObject FaceAView, FaceBView;
-
 #if UNITY_EDITOR
     public void SetFaces((GameObject, GameObject) one, (GameObject, GameObject) two)
     {
         (m_FaceA, m_FaceB) = jointSide ? one : two;
         if (bones.Length != 4 || bones.Contains(null))
             throw new UnityException("Bones Not Updated Correctly");
-        FaceAView = m_FaceA;
-        FaceBView = m_FaceB;
     }
 
     public bool SameSide(GlowStick other) => jointSide == other.jointSide;
 #endif
 
-#if UNITY_EDITOR
     private void Update()
-#else
-    private void FixedUpdate()
-#endif
     {
         if (m_FaceA == null || m_FaceB == null) return;
 
@@ -52,60 +44,55 @@ public class GlowStick : MonoBehaviour
         var pJoint = transform.parent.position; // the location of the joint, NOT always the same as mid (see below)
         var pA = m_FaceA.transform.position;
         var pB = m_FaceB.transform.position;
-        var mid = (pA + pB) / 2; // the average between the central coordinates of squares A and B, abbrieviate below as M
         var nA = m_FaceA.transform.up;
         var nB = m_FaceB.transform.up;
-
-        var a2B = pB - pA;
-
-        var m2J = pJoint - mid;
 
         var nJ2A = (pA - pJoint).normalized;
         var nJ2B = (pB - pJoint).normalized;
         transform.localPosition = Vector3.zero;
 
-        // always pin the ends
-        // bones[0].position = pJoint + nJ2A * halfLength + nA * elevation;
-        // bones[0].up = -nJ2A;
-        // bones[4].position = pJoint + nJ2B * halfLength + nB * elevation;
-        // bones[4].up = nJ2B;
+        // always pin the starting tip
+        bones[0].position = pJoint + nJ2A * halfLength + nA * elevation;
+        bones[0].up = -nJ2A;
 
-        // folded over, tricky case...
-        if (a2B.sqrMagnitude < critical)
+        // decide midpoint and ending tip depending on folding case
+        var tJ = Vector3.Cross(nA, -nJ2A); // tangent along joint
+        var angFold = Vector3.SignedAngle(nJ2A, nJ2B, tJ);
+        var nJ = Quaternion.AngleAxis(angFold / 2f, tJ) * -nJ2A;
+
+        if (angFold > 5f && angFold < 89f)
+            throw new UnityException("Cannot fold inwards");
+
+        // folding outwards (270 degrees ~ 0 degrees, plus some margin)
+        if (angFold > -89f && angFold < 5f)
+        {
+            // folding beyond 270 degrees
+            bones[1].position = pJoint + nA * elevation;
+            bones[2].position = pJoint + nJ * elevation;
+            bones[3].position = pJoint + nB * elevation;
+            bones[1].up = (bones[2].position - bones[1].position).normalized;
+            bones[2].up = (bones[3].position - bones[2].position).normalized;
+            bones[3].up = nJ2B;
+        }
+        // folding outwards (180 ~ 270 degrees)
+        else if (angFold <= -89f)
         {
             bones[1].position = pJoint + nA * elevation;
-            bones[1].up = -nJ2A;
-            bones[2].up = (nJ2B - nJ2A).normalized;
+            bones[2].position = pJoint + nJ * elevation;
             bones[3].position = pJoint + nB * elevation;
+            bones[1].up = (bones[2].position - bones[1].position).normalized;
+            bones[2].up = (bones[3].position - bones[2].position).normalized;
             bones[3].up = nJ2B;
-            bones[2].position = (bones[1].position + bones[3].position) / 2f + -nJ2A * elevation;
-            return;
         }
-
-        // significant fold angle to proceed with rest of the placement
-        // this way the glowstick doesn't glitch at flat-ish angles
-        var nJ2M = -m2J.normalized;
-        var indicator = Vector3.Dot(nJ2M, nA);
-
-        if (indicator > 0 || indicator < 0 && indicator < (critical - 1))
-        {
-            // folding inwards
-            bones[1].position = pJoint + nJ2A * (elevation + squareRenderSettings.margin) + nA * elevation;
-            bones[1].up = -nJ2A;
-            bones[2].up = (nJ2B - nJ2A).normalized;
-            bones[3].position = pJoint + nJ2B * (elevation + squareRenderSettings.margin) + nB * elevation;
-            bones[3].up = nJ2B;
-            bones[2].position = (bones[1].position + bones[3].position) / 2f;
-        }
+        // folding inwards
         else
         {
-            // folding outwards
-            bones[1].position = pJoint + nA * elevation;
-            bones[1].up = -nJ2A;
-            bones[2].up = (nJ2B - nJ2A).normalized;
-            bones[3].position = pJoint + nB * elevation;
-            bones[3].up = nJ2B;
+            bones[1].position = pJoint + nA * elevation + nJ2A * (elevation + squareRenderSettings.margin);
+            bones[3].position = pJoint + nB * elevation + nJ2B * (elevation + squareRenderSettings.margin);
             bones[2].position = (bones[1].position + bones[3].position) / 2f;
+            bones[1].up = (bones[2].position - bones[1].position).normalized;
+            bones[2].up = (bones[3].position - bones[1].position).normalized;
+            bones[3].up = nJ2B;
         }
     }
 }
