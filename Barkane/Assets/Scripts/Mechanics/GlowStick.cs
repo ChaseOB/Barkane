cd20c757af5420bf935ce258e9905a9acb6c48d4
alanrtao <alanrtao@outlook.com>
2022-11-24 21:16:24 -0500

    glowstick cases cleanup

--- Barkane/Assets/Scripts/Mechanics/GlowStick.cs
+++ Barkane/Assets/Scripts/Mechanics/GlowStick.cs
@@ -86,7 +86,7 @@ public class GlowStick : MonoBehaviour, IRefreshable
                 transform.worldToLocalMatrix.MultiplyVector(g.nJ),
                 transform.worldToLocalMatrix.MultiplyVector(g.tJ),
                 1 + 3 * settings.resolution, settings, shrinkCorrection);
-        } else if (g.a2b < -20f) // bending outwards but not extreme
+        } else // bending outwards
         {
             // near joint on side A
             var jA = transform.worldToLocalMatrix.MultiplyPoint(g.pJ + g.nJ2A * margin + g.nA * settings.elevation);
@@ -112,33 +112,7 @@ public class GlowStick : MonoBehaviour, IRefreshable
                 transform.worldToLocalMatrix.MultiplyVector(g.nB),
                 transform.worldToLocalMatrix.MultiplyVector(g.tJ),
                 1 + 3 * settings.resolution, settings);
-        } else // bending outwards, extreme; inner nodes no longer hug towards the squares
-        {
-            // near joint on side A
-            var jA = transform.worldToLocalMatrix.MultiplyPoint(g.pJ + g.nA * settings.elevation);
-            Ring(
-                ref vs, ref ns,
-                jA,
-                transform.worldToLocalMatrix.MultiplyVector(g.nA),
-                transform.worldToLocalMatrix.MultiplyVector(g.tJ),
-                1 + settings.resolution, settings);
-            // joint
-            var j = transform.worldToLocalMatrix.MultiplyPoint(g.pJ + g.nJ * settings.elevation);
-            Ring(
-                ref vs, ref ns,
-                j,
-                transform.worldToLocalMatrix.MultiplyVector(g.nJ),
-                transform.worldToLocalMatrix.MultiplyVector(g.tJ),
-                1 + 2 * settings.resolution, settings);
-            // near joint on side B
-            var jB = transform.worldToLocalMatrix.MultiplyPoint(g.pJ + g.nB * settings.elevation);
-            Ring(
-                ref vs, ref ns,
-                jB,
-                transform.worldToLocalMatrix.MultiplyVector(g.nB),
-                transform.worldToLocalMatrix.MultiplyVector(g.tJ),
-                1 + 3 * settings.resolution, settings);
-        }
+        } 
         // head B
         vs[vs.Length - 1] = transform.worldToLocalMatrix.MultiplyPoint(g.pJ + g.nJ2B * (settings.halfLength + margin) + g.nB * settings.elevation);
         ns[ns.Length - 1] = transform.worldToLocalMatrix.MultiplyVector(g.nJ2B);
